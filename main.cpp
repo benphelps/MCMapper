ca2a2a5c9d5fa83b5d111f224ed2f759dc51a9df
Ben Phelps <bphelpsen@gmail.com>
2011-01-28 07:50:45 -0600

    Added windows support, added density file generation, variable name cleanup.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*
+ *  main.cpp
+ *  MCMapper
+ *
+ *  Created by Ben Phelps on 1/25/11.
+ *  Copyright 2011. All rights reserved.
+ *
+ */
+
+
 // libs
 #include "pngwriter.h"
 #include "base64.h"
@@ -12,6 +22,12 @@
 #include <map>
 #include <string>
 
+#ifndef _WIN32	// windows has 'safe' type functions
+#define	sprintf_s(buffer, buffer_size, stringbuffer, ...) (sprintf(buffer, stringbuffer, __VA_ARGS__))
+#define	vsprintf_s(buffer, stringbuffer, ...) (vsprintf(buffer, stringbuffer, __VA_ARGS__))
+#define	sscanf_s(buffer, stringbuffer, ...) (sscanf(buffer, stringbuffer, __VA_ARGS__))
+#endif
+
 
 using namespace std;
 
@@ -20,7 +36,7 @@ struct point {
 };
 
 
-// This is not cross platform, need to find a method for windows.
+#ifndef _WIN32
 int getdir (string dir, vector<string> &files)
 {
     DIR *dp;
@@ -38,12 +54,28 @@ int getdir (string dir, vector<string> &files)
     closedir(dp);
     return 0;
 }
+#else
+#include <Windows.h>
+void getdir (string dir, vector<string> &files)
+{
+	HANDLE hFind;
+	WIN32_FIND_DATA FindFileData;
+	int numfiles = 0;
+	string Query = dir + "*.bin";
+	if((hFind = FindFirstFile(Query.c_str(), &FindFileData)) != INVALID_HANDLE_VALUE){
+		do{
+			files.push_back( string( FindFileData.cFileName ) );
+		}while(FindNextFile(hFind, &FindFileData));
+		FindClose(hFind);
+	}
+}
+#endif
 
 int main(int argc, char *argv[])
 {
 
-	string chunkDirectory = "world";
-	string imageDirectory = "images";
+	string chunkDirectory = "world/";
+	string imageDirectory = "images/";
 	string outputColor = "color";
 	string colorFile = "colors.txt";
 	int compressionLevel = 5;
@@ -51,6 +83,7 @@ int main(int argc, char *argv[])
 	bool createBlank = true;
 	bool greyScale = false;
 	bool onlyHeightmap = false;
+	bool densityFile = false;
 	
 	// super awesome custom user input parser
 	for (int i = 0; i < argc; i++) {
@@ -80,9 +113,12 @@ int main(int argc, char *argv[])
 		}
 		else if ( strcmp(argv[i], "-c") == 0) {
 			int level = atoi(argv[i+1]);
-			if(level > 0 and level <= 9){
+			if(level >= 0 and level <= 9){
 				compressionLevel = level;
 			}
+		}
+		else if ( strcmp(argv[i], "-d") == 0) {
+			densityFile = true;
 		}
 		else if ( strcmp(argv[i], "-h") == 0 or strcmp(argv[i], "--help") == 0 ) {
 			cout << "\nUsage:\n  MCMapper -i <worldDirectory> -o <imageDirectory> [-gs] [-nb] [-q] [-h]\n\n";
@@ -96,10 +132,11 @@ int main(int argc, char *argv[])
 			cout << "            Default: colors.txt (included)\n";
 			cout << "-nb         Preserve an already existing 'blank.png'.\n";
 			cout << "-q          Suppress output.\n";
-			cout << "-h --help   Output this message.\n\n";
+			cout << "-h --help   Shows this message.\n\n";
 			cout << "Output Options:\n";
-			cout << "-hm         Save only the height map (buggy).\n";
-			cout << "-gs         Save images in greyscale.\n\n";
+			cout << "-hm         Save only the height map.\n";
+			cout << "-gs         Save images in greyscale.\n";
+			cout << "-d          Save a desnity file along with the images.\n\n";
 			return 0;
 		}
 		else {
@@ -115,22 +152,28 @@ int main(int argc, char *argv[])
 	if(not shush) cout << "Compression Level: " << compressionLevel << "\n";
 	if(greyScale) cout << "Saving images in greyscale.\n";
 	if(onlyHeightmap) cout << "Saving image height map only.\n";
+
+	// check for colors files
+	ifstream color_file(colorFile.c_str(), std::ios::in);
+	if( !color_file.good() )
+	{
+		color_file.close();
+		printf( "Could not load colors files %s! File is missing.\n", colorFile.c_str() );
+		return 0;
+	}
 	
-	if(not shush) cout << "Loading Colors...\n";
-	// load colors
-	ifstream color_file( colorFile.c_str() );
-	std::map<int, point> m;
+	std::map<int, point> colors;
 	point p;
 	int id;
 	int colorCount = 0;
 	while(color_file >> id >> p.r >> p.g >> p.b)
 	{
-		m[id] = p;
+		colors[id] = p;
 		colorCount++;
 	}
 	
 	if(colorCount == 0){
-		cout << "Could not load colors from colors.txt! File is missing or wrong format.\n";
+		cout << "Could not load colors file is wrong format.\n";
 		return 0;
 	}
 	
@@ -140,6 +183,7 @@ int main(int argc, char *argv[])
     getdir(dir,files);
 	
 	int total_chunks = 0;
+	unsigned int density[128];
 	
 	if(not shush) cout << "Building heightmaps and creating chunk images...\n";
 	
@@ -148,22 +192,21 @@ int main(int argc, char *argv[])
 		string file = files[i];
 
 		int chunkX, chunkY, chunkZ;
-		int cordMatch = sscanf( file.c_str(), "X%d_Y%d_Z%d.bin", &chunkX, &chunkY, &chunkZ );
+		int cordMatch = sscanf_s( file.c_str(), "X%d_Y%d_Z%d.bin", &chunkX, &chunkY, &chunkZ );
 
 		// check if we are a proper chunk file
 		if (cordMatch != 3){
-			cout << "Warning: " << file.c_str() << " does not have proper file naming.  Ignoring.\n";
 			continue;
 		}
 		
 		int byte = 0;
-		int hm[16][16];
-		int depth[16][16];
+		int heightMap[16][16];
+		int depthMap[16][16];
 		
 		// read in the chunk data
 		// 16*16*128 bytes
 		ifstream chunk;
-		string fopen = chunkDirectory + string("/") + file;
+		string fopen = chunkDirectory + file;
 		chunk.open (fopen.c_str(), ios::binary);
 
 		char * data;
@@ -184,8 +227,9 @@ int main(int argc, char *argv[])
 					
 					// set highest not air block
 					if (block != 0) {
-						hm[x][z] = block;
-						depth[x][z] = y;
+						heightMap[x][z] = block;
+						depthMap[x][z] = y;
+						density[block] = density[block] + 1;
 						topBlock = block;
 					}
 					
@@ -198,7 +242,7 @@ int main(int argc, char *argv[])
 				// if we found water at this X, Z !AND! the top block is water
 				// then set the depth to the int we set in the loop
 				if (bFoundWater == true and topBlock == 8 or topBlock == 9) {
-					depth[x][z] = waterDepth;
+					depthMap[x][z] = waterDepth;
 				}
 			}
 		}
@@ -208,34 +252,34 @@ int main(int argc, char *argv[])
 
 		// the file name of the new png
 		char pngName[16];
-		sprintf( pngName, "%i-%i.png", chunkX, chunkZ );
-		string out = imageDirectory + "/" + pngName;
+		sprintf_s( pngName, 16, "%i-%i.png", chunkX, chunkZ );
+		string fileName = imageDirectory + pngName;
 		
 		// create the image
-		pngwriter image(16, 16, 1.0, out.c_str() );
+		pngwriter image(16, 16, 1.0, fileName.c_str() );
 		image.setcompressionlevel(compressionLevel);
 		
 		for (int x=0; x < 16; x++) { 
 			for (int z=0; z < 16; z++) {
-				int block = hm[x][z];
-				float dpth = depth[x][z];
+				int blockID = heightMap[x][z];
+				float depth = depthMap[x][z];
 				if (greyScale == true) {
-					float luma = ((((0.299*m[block].r) + (0.587*m[block].g) + (0.114*m[block].b)) * (dpth/128)) / 255.0f);
+					float luma = ((((0.299*colors[blockID].r) + (0.587*colors[blockID].g) + (0.114*colors[blockID].b)) * (depth/128)) / 255.0f);
 					image.plot(x+1, z+1, luma, luma, luma);
 				}
 				else if (onlyHeightmap == true) {
-					float height = (dpth*2) / 255.0f;
+					float height = (depth*2) / 255.0f;
 					image.plot(x+1, z+1, height, height, height);
 				}
 				else {
-					float r = m[block].r * (dpth/128) / 255.0f;
-					float g = m[block].g * (dpth/128) / 255.0f;
-					float b = m[block].b * (dpth/128) / 255.0f;
+					float r = (colors[blockID].r * (depth/128)) / 255.0f;
+					float g = (colors[blockID].g * (depth/128)) / 255.0f;
+					float b = (colors[blockID].b * (depth/128)) / 255.0f;
 					image.plot(x+1, z+1, r, g, b);
 				}
 			}
 		}
-
+		
 		image.close();
 		
 		total_chunks++;
@@ -253,9 +297,29 @@ int main(int argc, char *argv[])
 	}
 	
 	
+	if (densityFile) {
+		float totalBlocks;
+		char * format;
+		format = new char [16];
+		
+		for(int i = 0; i < 128; i++) {
+			totalBlocks += density[i];
+		}
+		
+		ofstream densityFileH("density.txt");
+		for(int i = 0; i < 128; i++) {
+			int blockDensity = density[i];
+			if (blockDensity > 0) {	
+				float percent = blockDensity / totalBlocks * 100.0f;
+				sprintf( format, "%i:%i:%f\n", i, blockDensity, percent);
+				densityFileH << format;
+			}
+		}
+		densityFileH.close();
+	}
+	
 	if(not shush) cout << "Processed " << total_chunks << " chunks.\n";
 	if(not shush) cout << "Done!\n\n";
 	
 	return 0;
 }
-
